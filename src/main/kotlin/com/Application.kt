1217dc26864993d958cd4586230e3483ec7b0a7b
Markus Byremo <markuswb77@gmail.com>
2024-05-01 11:04:40 +0200

    first initial commit

---
+++ src/main/kotlin/com/Application.kt
@@ -0,0 +1,15 @@
+package com
+
+import com.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
