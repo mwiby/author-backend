d5c2ddbf3b0c7f8f7a9de1386b0ad927285a7602
Markus Byremo <markuswb77@gmail.com>
2024-05-05 13:17:38 +0200

    Added data classes for Author app

---
+++ src/main/kotlin/com/dto/DataClasses.kt
@@ -0,0 +1,12 @@
+package com.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Author(val id: Int? = null, val name: String)
+
+@Serializable
+data class Post(val id: Int? = null, val authorId: Int, val title: String, val content: String)
+
+@Serializable
+data class Comment(val id: Int? = null, val postId: Int, val content: String)
