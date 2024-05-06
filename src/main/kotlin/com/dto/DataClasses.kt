c7d3ed4a31932106ad6e3a6fef20267401b7d7a0
Markus Byremo <markuswb77@gmail.com>
2024-05-06 09:24:27 +0200

    adjustment on data classes and added list comments for singel post

--- src/main/kotlin/com/dto/DataClasses.kt
+++ src/main/kotlin/com/dto/DataClasses.kt
@@ -3,10 +3,23 @@ package com.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Author(val id: Int? = null, val name: String)
+data class Author(
+    val id: Int? = null,
+    val name: String
+)
 
 @Serializable
-data class Post(val id: Int? = null, val authorId: Int, val title: String, val content: String)
+data class Post(
+    val id: Int? = null,
+    val authorId: Int,
+    val title: String,
+    val content: String,
+    val comments: List<Comment>? = null
+)
 
 @Serializable
-data class Comment(val id: Int? = null, val postId: Int, val content: String)
+data class Comment(
+    val id: Int? = null,
+    val postId: Int,
+    val content: String
+)
